515bedb8f7df2bc5cd05e6fa1927bd0b8660d101
Yellow <570700160@qq.com>
2017-11-23 10:46:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yellow/idiotplayer/MainActivity.java
@@ -0,0 +1,298 @@
+package com.example.yellow.idiotplayer;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Parcel;
+import android.os.StrictMode;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private boolean isPlaying=false;
+    private MediaPlayer mPlayer;
+    private TextView playtstate;
+    private SeekBar bar;
+    private MusicService myservice;
+    private boolean isFirst=true;
+    private IBinder mBinder;
+    private boolean hasPermission=true;
+    private float rotateF=0f;
+
+    private static final int REQUEST_EXTERNAL_STORAGE=1;
+    private static String[] PERMISSION_STORAGE={
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //myservice=new MusicService();
+
+        initAndBind();
+
+        verifyPermission(this);
+    }
+
+    public void initAndBind(){
+        //mPlayer=MediaPlayer.create(this,R.raw.melt);
+        playtstate=(TextView)findViewById(R.id.play_state);
+        playtstate.setText("READY");
+
+        Intent intent=new Intent(this,MusicService.class);
+        bindService(intent,sc,BIND_AUTO_CREATE);
+        startService(intent);
+    }
+    public void initBar(){
+        bar=(SeekBar)findViewById(R.id.seekbar_music);
+        bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                try{
+                    int code=104;//SeekTo
+                    Parcel data=Parcel.obtain();
+                    data.writeInt(bar.getProgress()*1000);
+                    mBinder.transact(code,data,Parcel.obtain(),0);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Parcel reply=Parcel.obtain();
+        try{
+            int code=106;//获取总时长
+            mBinder.transact(code,Parcel.obtain(),reply,0);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        int len=reply.readInt()/1000;//=myservice.getAllTime()/1000;//get到的是ms,换为s
+        bar.setMax(len);
+        TextView timeAll=(TextView)findViewById(R.id.time_all);
+        timeAll.setText(intToTime(len));
+
+        final Handler handler=new Handler(){
+            @Override
+            public void handleMessage(Message msg){
+                super.handleMessage(msg);
+                switch(msg.what){
+                    case 111:
+                        //更新UI
+                        if(sc!=null && isPlaying){
+                            updateBar();
+
+                            rotateImg();
+                        }
+                        break;
+                }
+            }
+        };
+        Thread thread=new Thread(){
+            @Override
+            public void run(){
+                while(true){
+                    try{
+                        Thread.sleep(300);
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    if(sc!=null&&hasPermission) handler.obtainMessage(111).sendToTarget();
+                }
+            }
+        };
+        thread.start();
+
+        /*final Thread mThread=new Thread() {
+            @Override
+            public void run() {
+                if(sc!=null && isPlaying){
+                    Parcel reply=Parcel.obtain();
+                    try{
+                        int code=105;//获取当前播放时间
+                        mBinder.transact(code,Parcel.obtain(),reply,0);
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                    int len=reply.readInt();//=myservice.getCurrentTime()/1000;//get到的是ms,换为s
+                    bar.setProgress(len);
+                    TextView tv=(TextView)findViewById(R.id.time_current);
+                    tv.setText(intToTime(len));
+                   // handler.postDelayed(Thread,1000);//每次延迟1000ms再启动线程
+                }
+            }
+        };*/
+    }
+
+    private ServiceConnection sc=new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            //myservice=((MusicService.MusicBinder) iBinder ).getService();
+            mBinder=iBinder;
+            //myservice.setPlayer(mPlayer);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            sc=null;
+        }
+    };
+
+
+    public void changePlayState(View view){
+        if(isFirst) initBar();
+        Button btn=(Button)findViewById(R.id.play_btn);
+        if(isPlaying) {
+            //myservice.pause();
+            playtstate.setText("PAUSED");
+            isPlaying=false;
+            btn.setText("PLAY");
+        }
+        else {
+            //myservice.play();
+            playtstate.setText("PLAYING");
+            isPlaying=true;
+            btn.setText("PAUSE");
+        }
+        try{
+            int code=101;
+            mBinder.transact(code,Parcel.obtain(),Parcel.obtain(),0);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void stopPlay(View view){
+        ImageButton iv=(ImageButton)findViewById(R.id.img_cd);
+        rotateF=0f;
+        iv.setRotation(rotateF);
+
+        Button btn=(Button)findViewById(R.id.play_btn);
+        btn.setText("PLAY");
+        isPlaying=false;
+        //myservice.stop();
+        try{
+            int code=102;
+            Parcel data=Parcel.obtain();
+            Parcel reply=Parcel.obtain();
+            mBinder.transact(code,data,reply,0);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        //isFirst=true;
+        playtstate.setText("READY");
+        TextView currentTime=(TextView)findViewById(R.id.time_current);
+        currentTime.setText("00:00");
+        bar.setProgress(0);
+    }
+    public void quitApp(View view){
+        unbindService(sc);
+        sc=null;
+        //myservice.stop();
+        try{
+            int code=103;//quit
+            mBinder.transact(code,Parcel.obtain(),Parcel.obtain(),0);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        this.finish();
+        System.exit(0);
+    }
+    public void randomNext(View view){
+        try{
+            int code=107;//next
+            Parcel reply=Parcel.obtain();
+            mBinder.transact(code,Parcel.obtain(),reply,0);
+            TextView tv=(TextView)findViewById(R.id.song_name_singer);
+            tv.setText(reply.readString());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public String intToTime(int len){
+        int min;
+        int sec;
+        min=len/60;
+        sec=len%60;
+        String timem;
+        String times;
+        if(min<10) timem="0"+min;
+        else timem=""+min;
+        if(sec<10) times="0"+sec;
+        else times=""+sec;
+        return timem+":"+times;
+    }
+
+    public void updateBar(){
+        Parcel reply=Parcel.obtain();
+        try{
+            int code=105;//获取当前播放时间
+            mBinder.transact(code,Parcel.obtain(),reply,0);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        int len=reply.readInt();//=myservice.getCurrentTime()/1000;//get到的是ms,换为s
+        bar.setProgress(len/1000);
+        TextView tv=(TextView)findViewById(R.id.time_current);
+        tv.setText(intToTime(len/1000));
+    }
+    public void rotateImg(){
+        ImageButton iv=(ImageButton)findViewById(R.id.img_cd);
+        rotateF+=1f;
+        if(rotateF==360f) rotateF=0f;
+        //iv.setRotation(rotateF);
+        RotateAnimation rotate=new RotateAnimation(rotateF,rotateF+1f, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        rotate.setDuration(298);
+        rotate.setFillAfter(true);
+        iv.setAnimation(rotate);
+    }
+
+    public void verifyPermission(Activity activity){
+        try{
+            int permission= ActivityCompat.checkSelfPermission(activity,"android.permission.READ_EXTERNAL_STORAGE");
+            if(permission!= PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(activity,PERMISSION_STORAGE,1);
+            }
+            else hasPermission=true;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permission[],int[] grantResults){
+        if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            //permission granted
+        }
+        else{
+            //permission not set
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/yellow/idiotplayer/MusicService.java
@@ -0,0 +1,181 @@
+package com.example.yellow.idiotplayer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.os.Parcel;
+
+import java.util.Random;
+
+/**
+ * Created by Yellow on 2017-11-22.
+ */
+
+public class MusicService extends Service {
+    private MediaPlayer mPlayer;
+    private String[] Songs={"melt.mp3","One Republic - Counting Stars.mp3","LINKIN PARK - Numb.mp3","周深 - 大鱼.mp3",
+    "金玟岐 - 山丘.mp3","少司命 - 临安记忆.mp3","张信哲 - 焚情.mp3"};
+    private String SDPath= Environment.getExternalStorageDirectory().toString();
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        mPlayer=MediaPlayer.create(this,R.raw.melt);
+
+        /*try{
+            mPlayer.setDataSource("/kgmusic/download/melt.mp3");
+            mPlayer.prepare();
+
+            mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    Random rand=new Random();
+                    int index=rand.nextInt(7);
+                    try{
+                        mPlayer.reset();
+                        mPlayer.setDataSource("file://data/kgmusic/download/"+Songs[index]);
+                        mPlayer.prepare();
+                        mPlayer.start();
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } catch (Exception e){
+            e.printStackTrace();
+        }*/
+    }
+    @Override
+    public int onStartCommand(Intent intent,int flags,int startId){
+        //mPlayer.start();
+        //return super.onStartCommand(intent,flags,startId);
+        return Service.START_STICKY;
+    }
+    @Override
+    public void onDestroy(){
+        mPlayer.stop();
+        mPlayer.release();
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent){
+        return new MusicBinder();
+    }
+    @Override
+    public boolean onUnbind(Intent intent){
+        mPlayer.stop();
+        return super.onUnbind(intent);
+    }
+
+    public class MusicBinder extends Binder{
+        MusicService getService(){
+            return MusicService.this;
+        }
+        @Override
+        protected boolean onTransact(int code, Parcel data, Parcel reply, int flags){
+            switch (code){
+                case 100:
+                    //play
+                    if(mPlayer!=null) mPlayer.start();
+                    break;
+                case 101:
+                    //pause
+                    if(mPlayer!=null&&mPlayer.isPlaying()) mPlayer.pause();
+                    else if(mPlayer!=null) mPlayer.start();
+                    break;
+                case 102:
+                    //stop
+                    if(mPlayer!=null){
+                        try{
+                            mPlayer.stop();
+                            mPlayer.reset();
+                            //mPlayer=MediaPlayer.create(this,R.raw.melt);
+                            setSong();
+                            mPlayer.prepare();
+                            mPlayer.seekTo(0);
+                        }catch(Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                case 103:
+                    //quit
+                    if(mPlayer!=null){
+                        mPlayer.stop();
+                        mPlayer.release();
+                    }
+                    break;
+                case 104:
+                    //seekTo
+                    if(mPlayer!=null){
+                        mPlayer.seekTo(data.readInt());
+                    }
+                    break;
+                case 105:
+                    //get media player position
+                    if(mPlayer!=null){
+                        int pos=mPlayer.getCurrentPosition();
+                        reply.writeInt(pos);
+                    }
+                    break;
+                case 106:
+                    //get duration
+                    if(mPlayer!=null){
+                        int len=mPlayer.getDuration();
+                        reply.writeInt(len);
+                    }
+                    break;
+                case 107:
+                    //next
+                    if(mPlayer!=null){
+                        Random rand=new Random();
+                        int index=rand.nextInt(7);
+                        try{
+                            mPlayer.reset();
+                            mPlayer.setDataSource("file://data/kgmusic/download/"+Songs[index]);
+                            mPlayer.prepare();
+                            mPlayer.start();
+
+                            //reply.writeString(Songs[index]);
+                            reply.writeString(SDPath);
+                        } catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+            }
+            return true;//super.onTransact(code,data,reply,flags)
+        }
+    }
+
+    public void play(){
+        mPlayer.start();
+    }
+    public void pause(){
+        mPlayer.pause();
+    }
+    public void stop(){
+        mPlayer.stop();
+    }
+    public int getAllTime(){
+        return mPlayer.getDuration();
+    }
+    public int getCurrentTime(){
+        return mPlayer.getCurrentPosition();
+    }
+    public void setTime(int position){
+        mPlayer.seekTo(position);
+    }
+    public void setPlayer(MediaPlayer mp){
+        mPlayer=mp;
+    }
+
+    public void setSong(){
+        mPlayer=MediaPlayer.create(this,R.raw.melt);
+    }
+
+}
